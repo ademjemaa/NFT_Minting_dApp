20424070422405999f1d0214ded6da0f99a85967
Moifek <moifekmaiza@gmail.com>
2022-04-18 16:46:52 +0100

    s2

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,212 @@
+import React, { FC, useState } from "react";
+import { logout, useGetAccountInfo } from "@elrondnetwork/dapp-core";
+import { Link } from "react-router-dom";
+import { dAppName } from "config";
+import { routeNames } from "routes";
+import { ProxyNetworkProvider } from "@elrondnetwork/erdjs-network-providers";
+import {
+  Transaction,
+  Nonce,
+  Balance,
+  GasPrice,
+  GasLimit,
+  TransactionPayload,
+  ChainID,
+  TransactionVersion,
+  Address,
+  NetworkConfig,
+  GasPriceModifier,
+  AbiRegistry,
+  SmartContractAbi,
+  SmartContract,
+} from "@elrondnetwork/erdjs";
+import promises from "fs";
+
+import logo from "./logo.png";
+import gif1 from "./gif1.gif";
+import AbiJson from "./elven-nft-minter.abi.json";
+import "./Homeindex.css";
+import { stringify } from "querystring";
+
+interface Props {
+  title: string;
+  initialCount: number;
+}
+
+const Home: FC<Props> = ({ title, initialCount }) => {
+  const [count, setCount] = useState(0);
+  const { address } = useGetAccountInfo();
+
+  const add = (factor = 1) => {
+    if (factor < 0) {
+      if (count > 0) setCount(count + factor);
+    } else {
+      setCount(count + factor);
+    }
+  };
+
+  
+
+  const abi = async () => {
+    let networkProvider = new ProxyNetworkProvider(
+      "https://devnet-gateway.elrond.com"
+    );
+
+    let networkConfig = await networkProvider.getNetworkConfig();
+    console.log(networkConfig.MinGasPrice);
+    console.log(networkConfig.ChainID);
+    let AbiText = JSON.stringify(AbiJson);
+    let jsonContent = AbiText;
+    console.log(AbiText);
+    console.log(AbiRegistry);
+    let json = JSON.parse(jsonContent);
+    let abi;
+    let abiRegistry = AbiRegistry.load(json).then(
+      (result) => (abi = new SmartContractAbi(result, ["test"]))
+    );
+    let contract = new SmartContract({
+      address: new Address(
+        "erd1qqqqqqqqqqqqqpgqdx22q4lg64w20fsscll2w5z5lc08whac5uhslwwwp7"
+      ),
+      abi: abi,
+    });
+    console.log(contract);
+    return contract;
+  };
+
+  abi().then((res) => {
+    console.log(res);
+  });
+
+  return (
+    <div
+      className="d-flex flex-fill align-items-center container"
+      style={{
+        backgroundColor: "#1e8520",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="row w-100 justify-content-between">
+        <div className="col-12 col-md-8 col-lg-5 mx-auto">
+          <img
+            src={String(logo)}
+            alt="Logo"
+            className="rounded mx-auto d-block"
+            style={{
+              width: "370px",
+              height: "320px",
+              margin: "15px",
+              padding: "25px",
+            }}
+          />
+
+          <div className="card shadow-sm rounded border border-warning p-4">
+            <div className="card-body text-center">
+              <img
+                src={String(gif1)}
+                className="rounded border rounded-circle mx-auto d-block"
+                style={{
+                  width: "320px",
+                  height: "320px",
+                  border: "10px",
+                  margin: "15px",
+                }}
+                alt="gif"
+              ></img>
+              <h2 className="mb-3" data-testid="title">
+                {dAppName}
+              </h2>
+              <div className="container">
+                {address ? (
+                  <button
+                    className="btn btn-warning btn-lg mt-3 text-white"
+                    style={{
+                      margin: "10px",
+                    }}
+                    onClick={() => add()}
+                  >
+                    +
+                  </button>
+                ) : null}
+                {address ? <h5>{count}</h5> : null}
+                {address ? (
+                  <button
+                    className="btn btn-secondary btn-lg mt-3 text-white"
+                    style={{
+                      margin: "5px",
+                    }}
+                    onClick={() => add(-1)}
+                  >
+                    -
+                  </button>
+                ) : null}
+              </div>
+              {address ? (
+                <button
+                  className="btn btn-warning btn-lg mt-3 text-white"
+                  style={{
+                    margin: "5px",
+                  }}
+                >
+                  Mint
+                </button>
+              ) : null}{" "}
+              {address ? (
+                <button
+                  className="btn btn-secondary btn-lg mt-3 text-white"
+                  style={{
+                    margin: "5px",
+                  }}
+                  onClick={() => {
+                    window.open(
+                      "https://deadrare.io/collection/ZEB-9k4d2j",
+                      "_blank"
+                    );
+                  }}
+                >
+                  DeadRare
+                </button>
+              ) : null}
+              <br />
+              <br />
+              <p className="mb-3">
+                This is the official N4P foresters NFTs Minting Site
+                <br /> Login using your Elrond wallet.
+              </p>
+              {!address ? (
+                <Link
+                  to={routeNames.unlock}
+                  className="btn btn-primary mt-3 text-white"
+                  data-testid="loginBtn"
+                >
+                  Login
+                </Link>
+              ) : null}
+            </div>
+            <img
+              src={String(logo)}
+              className="rounded float-right"
+              alt="..."
+            ></img>
+          </div>
+
+          <p
+            style={{
+              padding: "15px",
+              textAlign: "center",
+              alignContent: "center",
+              color: "white",
+            }}
+          >
+            We have set the gas limit to 1000 for the contract to successfully
+            mint your NFT. We recommend that you do not lower the gas limit.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
